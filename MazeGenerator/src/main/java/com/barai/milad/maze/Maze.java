45645be902feb112ee917a78fb41c48bc19608da
Milad Barai <ezbirmi@SE-00004209.ericsson.se>
2019-07-08 15:30:57 +0200

    Added getters to maze and implemented constructor, Changed to internal hashmap for ease of access to specific xy

--- MazeGenerator/src/main/java/com/barai/milad/maze/Maze.java
+++ MazeGenerator/src/main/java/com/barai/milad/maze/Maze.java
@@ -1,6 +1,9 @@
 package com.barai.milad.maze;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Set;
+import java.awt.Point;
 
 /**
  * Data structure for describing an entire maze
@@ -10,12 +13,42 @@ import java.util.HashSet;
  */
 public class Maze {
 	
-	MazeCell startingCell;
-	MazeCell endingCell;
-	HashSet<MazeCell> mazeCells;
+	private MazeCell startingCell;
+	private MazeCell endingCell;
+	private HashSet<MazeCell> mazeCells;
+	private HashMap<Point, MazeCell> pointToMazeCell;
 	
-	public Maze(){
-		
+	public Maze(MazeCell startingCell, MazeCell endingCell, HashMap<Point, MazeCell> pointToMazeCell){
+		this.startingCell = startingCell;
+		this.endingCell = endingCell;
+		this.pointToMazeCell = pointToMazeCell;
+		setupMazeCells(pointToMazeCell);
+	}
+
+	public MazeCell getStartingCell() {
+		return startingCell;
+	}
+
+	public MazeCell getEndingCell() {
+		return endingCell;
+	}
+
+	public HashSet<MazeCell> getMazeCells() {
+		return mazeCells;
+	}
+	
+	public MazeCell getMazeCell(int mazeX, int mazeY){
+		Point key = new Point(mazeX, mazeY);
+		return pointToMazeCell.get(key);
+	}
+	
+
+	private void setupMazeCells(HashMap<Point, MazeCell> pointToMazeCell) {
+		Set<Point> keys = pointToMazeCell.keySet();
+		mazeCells = new HashSet<MazeCell>();
+		for(Point key : keys){
+			mazeCells.add(pointToMazeCell.get(key));
+		}
 	}
 
 }
