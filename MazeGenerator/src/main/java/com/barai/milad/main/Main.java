2527a721560a8e413e56a37770d17de953e5cdd9
Milad Barai <miladbarai@gmail.com>
2019-07-09 16:25:45 +0200

    Main code changed to execute correctly

--- MazeGenerator/src/main/java/com/barai/milad/main/Main.java
+++ MazeGenerator/src/main/java/com/barai/milad/main/Main.java
@@ -1,14 +1,20 @@
 package com.barai.milad.main;
 
-import com.barai.milad.graphics.CellVisualizer;
+import com.barai.milad.converter.ModelToViewConverter;
+import com.barai.milad.graphics.MazeVisualizer;
 import com.barai.milad.graphics.WindowHandler;
+import com.barai.milad.maze.MazeGenerator;
 
 public class Main {
 
 	public static void main(String[] args) {
-		WindowHandler wh = new WindowHandler(500, 500);
-//		CellVisualizer test = new CellVisualizer();
-//		wh.drawCell(test);
+		int windowDim = 500;
+		int cellDim = 30;
+		
+		WindowHandler wh = new WindowHandler(windowDim, windowDim);
+		MazeVisualizer m = ModelToViewConverter.mazeToVisualizer(MazeGenerator.generateMaze(cellDim, cellDim), (windowDim-100)/cellDim, 50);
+		
+		wh.drawMaze(m);
 
 	}
 
